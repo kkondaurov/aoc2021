528ac3761701f9dc6a9918678277f8813326b54a
Konstantin Kondaurov <konstantin@kondaurov.com>
2021-12-02 18:08:40 +0400

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     match day {
         "1" => day01(filename),
 
+        "2" => day02(filename),
+
         &_ => println!("Day {} not implemented!", day)
     }
 }
@@ -23,6 +25,73 @@ fn parse_config(args: &[String]) -> (&str, &str) {
     let filename = &args[2];
 
     (day, filename)
+}
+
+
+fn day02(filename: &str) {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let result_part1 = day02_part1(contents.clone());
+    println!("Day 2 part 1 result: {}", result_part1);
+
+    let result_part2 = day02_part2(contents.clone());
+    println!("Day 2 part 2 result: {}", result_part2);
+}
+
+fn day02_part1(contents: String) -> i32 {
+    let mut position = 0;
+    let mut depth = 0;
+
+    for line in contents.lines() {
+        let mut iter = line.split_whitespace();
+        let command = iter.next().unwrap();
+        let arg = iter.next().unwrap().parse::<i32>().unwrap();
+        match command {
+            "forward" => position += arg,
+
+            "up" => if depth >= arg {
+                depth -= arg;
+            },
+
+            "down" => depth += arg,
+
+            &_ => println!("Command {} not implemented!", command)
+        }
+    }
+    return position * depth;
+}
+
+fn day02_part2(contents: String) -> i32 {
+    let mut position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in contents.lines() {
+        let mut iter = line.split_whitespace();
+        let command = iter.next().unwrap();
+        let arg = iter.next().unwrap().parse::<i32>().unwrap();
+        match command {
+            "forward" => {
+                position += arg;
+                let new_depth = depth + aim * arg;
+                if new_depth < 0 {
+                    depth = 0;
+                } else {
+                    depth = new_depth;
+                }
+            },
+
+            "up" => aim -= arg,
+
+            "down" => aim += arg,
+
+            &_ => println!("Command {} not implemented!", command)
+        }
+        println!("{} {}, position: {}, depth: {}, aim: {}", command, arg, position, depth, aim);
+    }
+
+    return position * depth;
 }
 
 fn day01(filename: &str) {
